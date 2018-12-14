99f21fc9f236f4e58eae0f87470e74a3cfa8354f
Victor Ferraz <victor.ferraz7@hotmail.com>
2018-12-14 04:17:38 +0000

    Dividindo a Query String

--- read-url.js
+++ read-url.js
@@ -1,7 +1,9 @@
-var http = require('http')
+var http = require ('http');
+var url = require ('url');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type':'text/html'});
-    res.write(req.url);
-    res.end (' < - foi o que estava escrito na url ');
+    var x = url.parse(req.url, true).query;
+    var info = x.nome+" "+x.sobrenome;
+    res.end(info);
 }).listen(process.env.PORT, process.env.IP);
